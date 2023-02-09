e9e1a417e2667f0fe01d8608a3fc57b4881e35c6
smmartprogrammer <partabrajp@gmail.com>
2023-02-10 04:20:12 +0500

    updating header

--- Components/Header.js
+++ Components/Header.js
@@ -1,17 +1,24 @@
-import Image from "next/image"
+import Image from "next/image";
+import { SearchIcon } from "@heroicons/react/solid";
 
 function Header() {
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10 ">
-     
-<div className="relative flex item-center h-10">
-<Image src="https://links.papareact.com/qd3" layout="fill" objectFit="contain" objectPosition="left"/>
-</div>
-<div></div>
-<div></div>
-
-     </header>
-  )
+      <div className="relative flex item-center h-10">
+        <Image
+          src="https://links.papareact.com/qd3"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+        />
+      </div>
+      <div>
+        <input type="text" placeholder="Start your search" />
+        <SearchIcon />
+      </div>
+      <div></div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
