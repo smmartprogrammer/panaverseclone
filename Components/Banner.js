c43f2b864aefdb295d28e696302e5aa6ea77a283
smmartprogrammer <partabrajp@gmail.com>
2023-02-11 00:05:45 +0500

    updated header and now creating the cover image

---
+++ Components/Banner.js
@@ -0,0 +1,16 @@
+import Image from "next/image"
+
+function Banner() {
+  return (
+    <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
+        <Image src="https://links.papareact.com/0fm " layout="fill" objectFit="cover"/>
+        <div className="absolute top=1/2">
+            <p>
+                Not sure where to go? Perfect.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default Banner
