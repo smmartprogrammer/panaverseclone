dc2e87bf6ce6a01a663810a1def761bbe3beb959
smmartprogrammer <partabrajp@gmail.com>
2023-02-11 16:34:04 +0500

    updated the git repo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 // import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import Header from "../Components/Header";
 import Banner from "../Components/Banner";
 import SmallCards from "../Components/SmallCards";
