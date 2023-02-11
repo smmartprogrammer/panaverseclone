c936de28f3e3e0e3e09df462bdbb984a45f3b1cd
smmartprogrammer <partabrajp@gmail.com>
2023-02-11 16:58:01 +0500

    update git

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Image from "next/legacy/image";
 import Header from "../Components/Header";
 import Banner from "../Components/Banner";
 import SmallCards from "../Components/SmallCards";
+import { type } from "os";
 
 export default function Home({ exploreData }) {
   return (
@@ -30,6 +31,8 @@ export default function Home({ exploreData }) {
     </div>
   );
 }
+
+// type exploreData= any
 
 async function getStaticProps() {
   const exploreData = await fetch("https://links.papareact.com/pyp").then(
