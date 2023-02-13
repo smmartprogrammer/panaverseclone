49ecd3f11557cd8387a7b7ca1fce5ca6c0713808
smmartprogrammer <partabrajp@gmail.com>
2023-02-14 01:43:14 +0500

    working on date ranger picker

--- Components/Header.js
+++ Components/Header.js
@@ -6,11 +6,26 @@ import {
   UsersIcon,
   SearchIcon,
 } from "@heroicons/react/solid";
-import { useState } from "react";
+import { use, useState } from "react";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker } from "react-date-range";
 
 function Header() {
-const [searchInput, setSearchInput] = useState("")
+  const [searchInput, setSearchInput] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
 
+  const handleSelect = (range) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
+  const selectionRanges = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
 
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10 ">
@@ -24,17 +39,15 @@ const [searchInput, setSearchInput] = useState("")
       </div>
       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
         <input
-
-        value={searchInput}
-        onChange={(e)=>setSearchInput(e.target.value)}
-
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
           type="text"
           placeholder="Start your search"
           className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-600"
         />
         <SearchIcon className="hidden md:inline-flex   h-8 bg-red-400 text-white p-2 rounded-full cursor-pointer md:mx-2 " />
       </div>
-      <div className="flex items-center justify-end text-gray-500 space-x-4 " >
+      <div className="flex items-center justify-end text-gray-500 space-x-4 ">
         <p className="hidden md:inline cursor-pointer">Become a host</p>
         <GlobeAltIcon className="h-6 cursor-pointer" />
         <div className="flex items-center space-x-2 border-2 p-2 rounded-full justify-end">
@@ -42,6 +55,16 @@ const [searchInput, setSearchInput] = useState("")
           <UserCircleIcon className="h-6" />
         </div>
       </div>
+      {searchInput && (
+        <div>
+          <DateRangePicker
+            ranges={[selectionRanges]}
+            minDate={new Date()}
+            rangeColors={["#fd5861"]}
+            onChange={handleSelect}
+          />
+        </div>
+      )}
     </header>
   );
 }
