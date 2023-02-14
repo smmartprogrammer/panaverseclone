7bed9aeeb601778fb49cfc07d05e8da97a50ceb6
smmartprogrammer <partabrajp@gmail.com>
2023-02-15 02:50:17 +0500

    adding search button including head and footer

---
+++ pages/search.tsx
@@ -0,0 +1,13 @@
+import Footer from "../Components/Footer"
+import Header from "../Components/Header"
+
+function Search() {
+  return (
+    <div>
+        <Header/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Search
