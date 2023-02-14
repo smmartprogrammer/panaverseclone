5af054772f91606fe14a5b35a81166999c225a81
smmartprogrammer <partabrajp@gmail.com>
2023-02-15 03:19:21 +0500

    creating reusable componnents

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,3 +1,4 @@
+import { Main } from "next/document"
 import Footer from "../Components/Footer"
 import Header from "../Components/Header"
 
@@ -5,6 +6,32 @@ function Search() {
   return (
     <div>
         <Header/>
+        <main className="flex">
+<section>
+    <p className="text-xs">
+        300+ Stays for 5 number of guests
+    </p>
+    <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in Mars</h1>
+    <div>
+        <p className="button">
+            Cancellation Flexibility
+        </p>
+        <p className="button">
+            Type of Place
+        </p>
+        <p className="button">
+            Price
+        </p>
+        <p className="button">
+            Rooms and Beds
+        </p>
+        <p className="button">
+            More Filters
+        </p>
+
+    </div>
+</section>
+        </main>
         <Footer/>
     </div>
   )
