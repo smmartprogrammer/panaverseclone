84b09fe7727188d8e050c611fc2262a2113bf751
smmartprogrammer <partabrajp@gmail.com>
2023-02-18 02:42:00 +0500

    working on search page

--- pages/search.tsx
+++ pages/search.tsx
@@ -11,11 +11,16 @@ function Search() {
           <p className="text-xs">300+ Stays for 5 number of guests</p>
           <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in Mars</h1>
           <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap ">
-            <p className="button">Cancellation Flexibility</p>
-            <p className="button">Type of Place</p>
-            <p className="button">Price</p>
-            <p className="button">Rooms and Beds</p>
-            <p className="button">More Filters</p>
+            <p className="px-4 py-2 border rounded-full cursor-pointer 
+        hover:shadow-lg active:scale-95 active:bg-gray-100 transition tranform duration-100 ease-out">Cancellation Flexibility</p>
+            <p className="px-4 py-2 border rounded-full cursor-pointer 
+        hover:shadow-lg active:scale-95 active:bg-gray-100 transition tranform duration-100 ease-out">Type of Place</p>
+            <p className="px-4 py-2 border rounded-full cursor-pointer 
+        hover:shadow-lg active:scale-95 active:bg-gray-100 transition tranform duration-100 ease-out">Price</p>
+            <p className="px-4 py-2 border rounded-full cursor-pointer 
+        hover:shadow-lg active:scale-95 active:bg-gray-100 transition tranform duration-100 ease-out">Rooms and Beds</p>
+            <p className="px-4 py-2 border rounded-full cursor-pointer 
+        hover:shadow-lg active:scale-95 active:bg-gray-100 transition tranform duration-100 ease-out">More Filters</p>
           </div>
         </section>
       </main>
