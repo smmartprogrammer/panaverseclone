51d4162364baaac55b60131eb94962d8f7d6c601
smmartprogrammer <partabrajp@gmail.com>
2023-02-20 22:17:21 +0500

    working on the funcationality part of the search page

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,46 +1,33 @@
 import { Main } from "next/document";
+import { useRouter } from "next/router";
 import Footer from "../Components/Footer";
 import Header from "../Components/Header";
+import {format} from "date-fns"
 
 function Search() {
+  const router = useRouter();
+
+  const { location, startDate, endDate, noOfGuests } = router.query;
+
+  const formattedStarDate = format(new Date(startDate), "dd MMMM yy")
+  const formattedEndDate = format(new Date(endDate), "dd MMMM yy")
+
+  const range = `${formattedStarDate} - ${formattedEndDate}`
+
+
   return (
     <div>
       <Header />
       <main className="flex">
-        <section>
-          <p className="text-xs">300+ Stays for 5 number of guests</p>
-          <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in Mars</h1>
+        <section className="flex-grow pt-14 px-6">
+          <p className="text-xs">300+ Stays for {noOfGuests} number of guests</p>
+          <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in {location}</h1>
           <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap ">
-            <p
-              className="px-4 py-2 border rounded-full cursor-pointer 
-        hover:shadow-lg active:scale-95 active:bg-gray-100 transition tranform duration-100 ease-out"
-            >
-              Cancellation Flexibility
-            </p>
-            <p
-              className="px-4 py-2 border rounded-full cursor-pointer 
-        hover:shadow-lg active:scale-95 active:bg-gray-100 transition tranform duration-100 ease-out"
-            >
-              Type of Place
-            </p>
-            <p
-              className="px-4 py-2 border rounded-full cursor-pointer 
-        hover:shadow-lg active:scale-95 active:bg-gray-100 transition tranform duration-100 ease-out"
-            >
-              Price
-            </p>
-            <p
-              className="px-4 py-2 border rounded-full cursor-pointer 
-        hover:shadow-lg active:scale-95 active:bg-gray-100 transition tranform duration-100 ease-out"
-            >
-              Rooms and Beds
-            </p>
-            <p
-              className="px-4 py-2 border rounded-full cursor-pointer 
-        hover:shadow-lg active:scale-95 active:bg-gray-100 transition tranform duration-100 ease-out"
-            >
-              More Filters
-            </p>
+            <p className="button">Cancellation Flexibility</p>
+            <p className="button">Type of Place</p>
+            <p className="button">Price</p>
+            <p className="button">Rooms and Beds</p>
+            <p className="button">More Filters</p>
           </div>
         </section>
       </main>
