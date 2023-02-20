357c045c2c7afa7bf5754621c206fdacb476827f
smmartprogrammer <partabrajp@gmail.com>
2023-02-21 03:12:06 +0500

    working on cards

--- pages/search.tsx
+++ pages/search.tsx
@@ -2,33 +2,34 @@ import { Main } from "next/document";
 import { useRouter } from "next/router";
 import Footer from "../Components/Footer";
 import Header from "../Components/Header";
-import {format} from "date-fns"
-import InfoCards from "../Components/InfoCards"
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { format } from "date-fns";
+import InfoCards from "../Components/InfoCards";
+import type { NextApiRequest, NextApiResponse } from "next";
 import { title } from "process";
-
 
 function Search({ searchResults }) {
   const router = useRouter();
 
-console.log(searchResults);
-
+  console.log(searchResults);
 
   const { location, startDate, endDate, noOfGuests } = router.query;
 
-  const formattedStarDate = format(new Date(startDate), "dd MMMM yy")
-  const formattedEndDate = format(new Date(endDate), "dd MMMM yy")
+  const formattedStarDate = format(new Date(startDate), "dd MMMM yy");
+  const formattedEndDate = format(new Date(endDate), "dd MMMM yy");
 
-  const range = `${formattedStarDate} - ${formattedEndDate}`
-
+  const range = `${formattedStarDate} - ${formattedEndDate}`;
 
   return (
     <div>
       <Header placeholder={`${location} | ${range} | ${noOfGuests} guests`} />
       <main className="flex">
         <section className="flex-grow pt-14 px-6">
-          <p className="text-xs">300+ Stays for {noOfGuests} number of guests</p>
-          <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in {location}</h1>
+          <p className="text-xs">
+            300+ Stays for {noOfGuests} number of guests
+          </p>
+          <h1 className="text-3xl font-semibold mt-2 mb-6">
+            Stays in {location}
+          </h1>
           <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap ">
             <p className="button">Cancellation Flexibility</p>
             <p className="button">Type of Place</p>
@@ -36,20 +37,22 @@ console.log(searchResults);
             <p className="button">Rooms and Beds</p>
             <p className="button">More Filters</p>
           </div>
-          {searchResults.map({img,location, title, description, star, price, total })  => 
-            ( 
-              <InfoCards
-              key={img}
-              img={img}
-              location={location}
-              title={title}
-              description={description}
-              star={star}
-              price={price}
-              total={total}
-              
-              />
-            ))}
+          <div className="flex flex-col">
+            {searchResults?.map(
+              ({ img, location, title, description, star, price, total }) => (
+                <InfoCards
+                  key={img}
+                  img={img}
+                  location={location}
+                  title={title}
+                  description={description}
+                  star={star}
+                  price={price}
+                  total={total}
+                />
+              )
+            )}
+          </div>
         </section>
       </main>
       <Footer />
@@ -60,15 +63,17 @@ console.log(searchResults);
 export default Search;
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 export async function getServerSideProps() {
- const searchResults = await fetch("https://links.papareact.com/isz").then(res => res.json()) 
+  const searchResults = await fetch("https://links.papareact.com/isz").then(
+    (res) => res.json()
+  );
 
-return {
-  props: {
-    searchResults,
-  }
-}
+  return {
+    props: {
+      searchResults,
+    },
+  };
 }
